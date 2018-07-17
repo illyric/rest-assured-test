818cb3426d6f4b7308bbfaa4dfc5889f0e5f4a88
Ilir Kosumi <ilirkosumi@gmail.com>
2018-07-17 21:09:12 +0200

    Typo fix

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 public class AppTest {
     @Test
-    public void makeSureGoodleIsUp() {
+    public void makeSureGoogleIsUp() {
         given().when().get("https://www.google.com").then().statusCode(200);
     }
 }
