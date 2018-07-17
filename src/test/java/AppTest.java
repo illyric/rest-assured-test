8e2a7565a7490b1d90d687dff2c8e4196ee4c276
Ilir Kosumi <ilirkosumi@gmail.com>
2018-07-17 20:22:38 +0200

    Add google GET test

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -3,5 +3,8 @@ import static com.jayway.restassured.RestAssured.given;
 import org.junit.Test;
 
 public class AppTest {
-
+    @Test
+    public void makeSureGoodleIsUp() {
+        given().when().get("https://www.google.com").then().statusCode(200);
+    }
 }
