7a3c3607e47b6b84bab6b8958038877c4afc42a3
Ilir Kosumi <ilirkosumi@gmail.com>
2018-07-17 21:56:11 +0200

    Better name for test class

--- src/test/java/GoogleTest.java
+++ src/test/java/GoogleTest.java
@@ -1,8 +1,8 @@
-import static com.jayway.restassured.RestAssured.given;
-
 import org.junit.Test;
 
-public class AppTest extends BaseTest {
+import static com.jayway.restassured.RestAssured.given;
+
+public class GoogleTest extends BaseTest {
     @Test
     public void makeSureGoogleIsUp() {
         given().when().get("https://www.google.com").then().statusCode(200);
