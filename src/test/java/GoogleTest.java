85543661e00d62fa887d891a93c59d8a68e358cd
Ilir Kosumi <ilirkosumi@gmail.com>
2018-07-17 23:08:17 +0200

    Ignore google tests for now

--- src/test/java/GoogleTest.java
+++ src/test/java/GoogleTest.java
@@ -1,9 +1,11 @@
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static com.jayway.restassured.RestAssured.given;
 
 public class GoogleTest extends BaseTest {
     @Test
+    @Ignore
     public void makeSureGoogleIsUp() {
         given().when().get("https://www.google.com").then().statusCode(200);
     }
