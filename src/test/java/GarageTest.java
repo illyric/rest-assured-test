16ccf88b6a15ad92831dfc692a0de2a6ac97f834
Ilir Kosumi <ilirkosumi@gmail.com>
2018-07-17 22:04:53 +0200

    Add new test

--- src/test/java/GarageTest.java
+++ src/test/java/GarageTest.java
@@ -7,4 +7,9 @@ public class GarageTest extends BaseTest {
     public void getGarageShouldReturnStatusCode200() {
         given().when().get("/garage").then().statusCode(200);
     }
+
+    @Test
+    public void getInvalidParkingSlotShouldReturnStatusCode404() {
+        given().when().get("/garage/slots/151").then().statusCode(404);
+    }
 }
