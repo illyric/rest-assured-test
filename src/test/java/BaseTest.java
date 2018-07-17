3e2497a9429da504946cf66a681a078729f65715
Ilir Kosumi <ilirkosumi@gmail.com>
2018-07-17 20:49:06 +0200

    Set up a BaseTest class

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,7 +1,32 @@
-package PACKAGE_NAME;
+import org.junit.BeforeClass;
 
-/**
- * Created by IKOSUMI on 17.07.2018.
- */
+import com.jayway.restassured.RestAssured;
+
 public class BaseTest {
+    // sets up RestAssured with a default target of "http://localhost:8080/rest-garage-sample"
+    // at the same time, allows to set a different web context using env. vars. ex.
+    // gradle test -Dserver.port=8081 -Dserver.host=http://example.com
+    // it will run the same suite of tests against an app deployed at "http://example.com:9000/rest-garage-sample"
+    // and thus enabling our tests to run against different environments
+    @BeforeClass
+    public static void setUp() {
+        String port = System.getProperty("server.port");
+        if (port == null) {
+            RestAssured.port = Integer.valueOf(8080);
+        } else {
+            RestAssured.port = Integer.valueOf(port);
+        }
+
+        String basePath = System.getProperty("server.base");
+        if (basePath == null) {
+            basePath = "/rest-garage-sample";
+        }
+        RestAssured.basePath = basePath;
+
+        String baseURI = System.getProperty("server.host");
+        if (baseURI == null) {
+            baseURI = "http://localhost";
+        }
+        RestAssured.baseURI = baseURI;
+    }
 }
