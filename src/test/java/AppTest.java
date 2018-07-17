8073f096f9b17791b687982637681966998010a7
Ilir Kosumi <ilirkosumi@gmail.com>
2018-07-17 21:45:16 +0200

    Use BaseTest

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -2,7 +2,7 @@ import static com.jayway.restassured.RestAssured.given;
 
 import org.junit.Test;
 
-public class AppTest {
+public class AppTest extends BaseTest {
     @Test
     public void makeSureGoogleIsUp() {
         given().when().get("https://www.google.com").then().statusCode(200);
