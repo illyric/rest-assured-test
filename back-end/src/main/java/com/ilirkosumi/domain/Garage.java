2970c0c37a76d64a303761864c8e6d635fbfd6a2
Ilir Kosumi <ilirkosumi@gmail.com>
2018-07-20 10:55:05 +0200

    Include Spring Data

--- back-end/src/main/java/com/ilirkosumi/domain/Garage.java
+++ back-end/src/main/java/com/ilirkosumi/domain/Garage.java
@@ -1,7 +1,22 @@
 package com.ilirkosumi.domain;
 
-public class Garage {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "garage")
+public class Garage implements Serializable {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NotBlank
     private final String name;
+    @NotBlank
     private final Info info;
 
     public Garage(String name, Info info) {
